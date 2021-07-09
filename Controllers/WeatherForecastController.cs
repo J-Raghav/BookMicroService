8b50c03a029fdcf95575259e542ec88884348b9e
Raghav Joshi <raghavjoshi789@gmail.com>
2021-07-09 10:30:31 +0530

    Added Book CRUD API

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using BookMicroService.Models;
+
 namespace BookMicroService.Controllers
 {
     [ApiController]
